a15a85de0df07590b97cf7e4be6ffd9d682d48b8
Fabiano Oliveira <fabiano.oliveira@ifood.com.br>
2018-04-03 18:21:59 -0300

    Fix docker runner before the first release

--- src/main/java/xyz/fabiano/spring/localstack/autoconfigure/LocalstackAutoConfiguration.java
+++ src/main/java/xyz/fabiano/spring/localstack/autoconfigure/LocalstackAutoConfiguration.java
@@ -1,9 +1,9 @@
 package xyz.fabiano.spring.localstack.autoconfigure;
 
-import cloud.localstack.docker.LocalstackDocker;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import xyz.fabiano.spring.localstack.LocalstackDocker;
 
 @Configuration
 @ConditionalOnProperty("spring.localstack.autoconfiguration")
